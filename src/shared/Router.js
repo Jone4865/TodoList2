91f3490645bffdd4d3017f006939f1359c4f8029
JongOne <cowhddnjsdl0@naver.com>
2022-08-03 09:27:24 +0900

    폴더구조 수정

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TodoList from "../components/TodoList/TodoList";
-import Detail from "../components/Detail/Detail";
+import Detail from "../components/pages/Detail";
 
 const Router = () => {
     return (
